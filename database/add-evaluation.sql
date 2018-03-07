73967c8d060dc63b9af8a4d60a50fde482773c72
Your Name <you@example.com>
2018-03-07 12:12:23 -0500

    vamos lac

--- database/add-evaluation.sql
+++ database/add-evaluation.sql
@@ -1,4 +1,3 @@
-Alter Table evaluation add column reply_admin VARCHAR(100) ;
-Alter Table evaluation add column reply_content TEXT;
-Alter Table evaluation add column updated_at  DATETIME;
+-- Alter Table evaluation add column reply_admin VARCHAR(100) ;
+-- Alter Table evaluation add column reply_content TEXT;
 Alter Table evaluation add column reply_date  DATETIME;
