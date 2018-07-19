24e17753bf1b202a4b95a8c4b602474e11f62bbe
Your Name <you@example.com>
2018-07-19 18:48:30 -0300

    a

--- app/Http/Controllers/ImportadorController.php
+++ app/Http/Controllers/ImportadorController.php
@@ -4142,7 +4142,7 @@ public function agregarNuevoNoGeo($book,$latLng){
 }
 
 	public function cleardb(Request $request){ //elimina datos de la tabla paises, provincias, partidos y places
-		$mode = config('MODE');
+		$mode = config('MODE', 'testing');
 		$result = ['mode' => $mode];
 		if (($mode !== null) && ($mode !== 'production'))  {
 			DB::statement('SET FOREIGN_KEY_CHECKS=0');
@@ -4162,7 +4162,7 @@ public function agregarNuevoNoGeo($book,$latLng){
 	}
 
 	public function getServerMode(Request $request){ //elimina datos de la tabla paises, provincias, partidos y places
-		$mode = config('MODE');
+		$mode = config('MODE', 'testing');
 		return(['mode' => $mode]);
 	}
 
