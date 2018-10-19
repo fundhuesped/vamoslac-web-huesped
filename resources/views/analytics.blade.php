f94e58b88437438e08fd6abeea06bf5713e8c4e3
Your Name <you@example.com>
2018-10-19 15:46:24 -0300

    stla

--- resources/views/analytics.blade.php
+++ resources/views/analytics.blade.php
@@ -5,6 +5,6 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
-gtag('config', 'UA-109530206-1');
+gtag('config', 'UA-107062237-1');
 </script>
 
