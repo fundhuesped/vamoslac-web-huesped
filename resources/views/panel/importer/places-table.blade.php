7562710921294e18a55ba0b5399024040cb3b523
Nicolas Eckell <nicolas@genosha.com.ar>
2020-05-20 15:49:23 -0300

    Bug fix de iconos de servicios al importar

--- resources/views/panel/importer/places-table.blade.php
+++ resources/views/panel/importer/places-table.blade.php
@@ -39,17 +39,22 @@
 				<td>{{$p['longitude']}}</td>
 				<td class="text-center">{{$p['aprobado']}}</td>
 				<td class="services2">
-					<img ng-hide="{{ $p['friendly_condones'] }}" ng-show="{{ $p['condones'] }}" title="Este lugar distribuye preservativos" src="../../images/condones.svg">
+					<img ng-show="{{ $p['condones'] && !$p['friendly_condones'] }}" title="Este lugar distribuye preservativos" src="../../images/condones.svg">
 					<img ng-show="{{ $p['friendly_condones'] }}" title="Este lugar distribuye preservativos" src="../../images/condones_friendly.svg">
-					<img ng-hide="{{ $p['friendly_prueba'] }}" ng-show="{{ $p['prueba'] }}" title="Este lugar puede hacer prueba de HIV" src="../../images/vih.svg">
+
+					<img ng-show="{{ $p['prueba'] && !$p['friendly_prueba'] }}" title="Este lugar puede hacer prueba de HIV" src="../../images/vih.svg">
 					<img ng-show="{{ $p['friendly_prueba'] }}" title="Este lugar puede hacer prueba de HIV" src="../../images/vih_friendly.svg">
-					<img ng-hide="{{ $p['friendly_mac'] }}" ng-show="{{ $p['mac'] }}" title="Este lugar cuenta con métodos anticonceptivos" src="../../images/mac.svg">
+
+					<img ng-show="{{ $p['mac'] && !$p['friendly_mac'] }}" title="Este lugar cuenta con métodos anticonceptivos" src="../../images/mac.svg">
 					<img ng-show="{{ $p['friendly_mac'] }}" title="Este lugar cuenta con métodos anticonceptivos" src="../../images/mac_friendly.svg">
-					<img ng-hide="{{ $p['friendly_ile'] }}" ng-show="{{ $p['ile'] }}" title="Este lugar cuenta con interrupción legal del embarazo" src="../../images/ile.svg">
+
+					<img ng-show="{{ $p['ile'] && !$p['friendly_ile'] }}" title="Este lugar cuenta con interrupción legal del embarazo" src="../../images/ile.svg">
 					<img ng-show="{{ $p['friendly_ile'] }}" title="Este lugar cuenta con interrupción legal del embarazo" src="../../images/ile_friendly.svg">
-					<img ng-hide="{{ $p['friendly_dc'] }}" ng-show="{{ $p['dc'] }}" title="Este lugar cuenta con detección dle cáncer" src="../../images/deteccion.svg">
+
+					<img ng-show="{{ $p['dc'] && !$p['friendly_dc'] }}" title="Este lugar cuenta con detección dle cáncer" src="../../images/deteccion.svg">
 					<img ng-show="{{ $p['friendly_dc'] }}" title="Este lugar cuenta con detección dle cáncer" src="../../images/deteccion_friendly.svg">
-					<img ng-hide="{{ $p['friendly_ssr'] }}" ng-show="{{ $p['ssr'] }}" title="Este lugar cuenta con servicios de salud sexual y reproductiva" src="../../images/salud.svg">
+
+					<img ng-show="{{ $p['ssr'] && !$p['friendly_ssr'] }}" title="Este lugar cuenta con servicios de salud sexual y reproductiva" src="../../images/salud.svg">
 					<img ng-show="{{ $p['friendly_ssr'] }}" title="Este lugar cuenta con servicios de salud sexual y reproductiva" src="../../images/salud_friendly.svg">
 				</td>
 			</tr>
