5c8624d0638296ea8c4b23f3bfe352cce2e5c420
Nicolas Eckell <nicolas@genosha.com.ar>
2020-06-23 18:28:43 -0300

    Fix del autocompletado

--- public/scripts/services/autocomplete.js
+++ public/scripts/services/autocomplete.js
@@ -15,19 +15,24 @@ dondev2App.factory('autocompleteService', function() {
       if(!document.getElementById('autocomplete'))
         return;
       autocomplete = new google.maps.places.Autocomplete(
-        document.getElementById('autocomplete'), {types: ['geocode']});
+        document.getElementById('autocomplete'), {types: ['(cities)']});
       autocomplete.setFields(['address_component']);
       autocomplete.addListener('place_changed', cb);
     },
     fillInCity: function(){
       var autocompleteForm = [];
       var place = autocomplete.getPlace();
-      for (var i = 0; i < place.address_components.length; i++) {
+      var n = place.address_components.length;
+      for (var i = 0; i < n; i++) {
         var addressType = place.address_components[i].types[0];
         if (componentForm[addressType]) {
           var val = place.address_components[i][componentForm[addressType]];
           autocompleteForm[i] = val;
         }
+      }
+      // Algunas ciudades vienen sin partidos :\ acá va un fix: copiamos el primer resultado
+      if(n == 3){
+        autocompleteForm.unshift(autocompleteForm[0]); 
       }
       return autocompleteForm;
     }
